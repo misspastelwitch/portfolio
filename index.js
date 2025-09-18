d9b2cfdfa44e46e8dbed2eac4b295a7727398df1
Stella <stellaclough@gmail.com>
2025-09-18 21:24:50 +0200

    Version 2.0

--- index.js
+++ index.js
@@ -18,3 +18,55 @@ window.addEventListener('scroll', () => {
   const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
   section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
 })
+
+/* Automatic scroll up*/
+$(function() {
+  $('.scroll-up').click (function() {
+    $('html, body').animate({scrollTop: document.body.scrollHeight }, 'slow');
+    return false;
+  });
+});
+
+
+/* Cursor */
+var canvas = document.getElementById('paint');
+var ctx = canvas.getContext('2d');
+ 
+var sketch = document.getElementById('sketch');
+var sketch_style = getComputedStyle(sketch);
+canvas.width = 500;
+canvas.height = 250;
+
+var mouse = {x: 0, y: 0};
+ 
+/* Mouse Capturing Work */
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
+console.log ('first 25 lines of JS are error-free')
+
+
+/* Drawing on Paint App */
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = "#0a2472";
+
+//ctx.strokeStyle = 
+//ctx.strokeStyle = document.settings.colour[1].value;
+ 
+canvas.addEventListener('mousedown', function(e) {
+    ctx.beginPath();
+    ctx.moveTo(mouse.x, mouse.y);
+ 
+    canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+ 
+canvas.addEventListener('mouseup', function() {
+    canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+ 
+var onPaint = function() {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+};
