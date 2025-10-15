c20f26f82e3db3f1ce451ced929e922e2f23ca8a
Stella <stellaclough@gmail.com>
2025-10-15 17:37:23 +0200

    Changed back to vanilla from Svelte

---
+++ index.js
@@ -0,0 +1,70 @@
+const imgContent = document.querySelectorAll(".img-content-hover");
+
+function showImgContent(e) {
+  for (var i = 0; i < imgContent.length; i++) {
+    x = e.pageX;
+    y = e.pageY;
+    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  }
+}
+
+document.addEventListener("mousemove", showImgContent);
+
+const [red, green, blue] = [69, 111, 225]
+const section1 = document.querySelector('.section1')
+window.addEventListener('scroll', () => {
+  const y = 1 + (window.scrollY || window.pageYOffset) / 150
+  const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+  section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`})
+
+/* Automatic scroll up*/
+$(function() {
+  $('.scroll-up').click (function() {
+    $('html, body').animate({scrollTop: document.body.scrollHeight }, 'slow');
+    return false;
+  });
+});
+
+
+/* Cursor */
+var canvas = document.getElementById('paint');
+var ctx = canvas.getContext('2d');
+ 
+var sketch = document.getElementById('sketch');
+var sketch_style = getComputedStyle(sketch);
+canvas.width = 500;
+canvas.height = 250;
+
+var mouse = {x: 0, y: 0};
+ 
+/* Mouse Capturing Work */
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
+console.log ('first 25 lines of JS are error-free')
+
+
+/* Drawing on Paint App */
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = "#0a2472";
+
+//ctx.strokeStyle = 
+//ctx.strokeStyle = document.settings.colour[1].value;
+ 
+canvas.addEventListener('mousedown', function(e) {
+    ctx.beginPath();
+    ctx.moveTo(mouse.x, mouse.y);
+ 
+    canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+ 
+canvas.addEventListener('mouseup', function() {
+    canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+ 
+var onPaint = function() {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+};
